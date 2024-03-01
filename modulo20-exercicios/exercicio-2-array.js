3e58dfa348326fb65c3db10315c4881b61494aa4
Humberto Gouveia <64939939+humbertogouveia@users.noreply.github.com>
2024-03-01 20:23:01 -0300

    Exercício de array finalizado

---
+++ modulo20-exercicios/exercicio-2-array.js
@@ -0,0 +1,6 @@
+let listaDeNumeros = [343,2342,757,342,8642]
+
+let maiorNumeroDaLista = Math.max(...listaDeNumeros)
+let indiceDoMaiorNumero = listaDeNumeros.indexOf(maiorNumeroDaLista)
+
+console.log(`O maior numero encontrado é o ${maiorNumeroDaLista} que está no índice [${indiceDoMaiorNumero}] `)
